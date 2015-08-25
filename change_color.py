308d2fc600f4d8134587be06ea3206e8ddfdd85d
skanupuru <sudha.kanupuru@3top.com>
2015-08-25 18:06:50 -0400

    colored text in python

---
+++ change_color.py
@@ -0,0 +1,17 @@
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
+
+def print_in_color(print_message):
+    print(color.WARNING + print_message + color.ENDC)
+
+print_in_color("well helloo there")
+
